fd371fe823b386f8f033a1a1de7412b586e96d0e
Mark Monday <mark.monday0504@gmail.com>
2025-11-03 15:55:48 -0500

    update included sources

--- src/services/bsky-profile-service.ts
+++ src/services/bsky-profile-service.ts
@@ -1,15 +1,17 @@
 import { getBskyPublicAgent } from '../connections/bsky-agent.js';
 
 const MAX_BSKY_PROFILES = 5;
-const USE_SECOND_PROFILE = ['bbc news'];
 
 export const getBskyProfile = async (sourceName: string, url: string) => {
 	const agent = getBskyPublicAgent();
-	const profileIndex = USE_SECOND_PROFILE.includes(sourceName.toLowerCase()) ? 1 : 0;
+	if (url === 'bbc.com') {
+		const bbcResp = await agent.searchActorsTypeahead({ q: 'bbcnews', limit: MAX_BSKY_PROFILES });
+		return bbcResp.data.actors[1];
+	}
 	const urlResp = await agent.searchActorsTypeahead({ q: url, limit: MAX_BSKY_PROFILES });
 	if (urlResp?.data?.actors.length) {
-		return urlResp.data.actors[profileIndex];
+		return urlResp.data.actors[0];
 	}
 	const nameResp = await agent.searchActorsTypeahead({ q: sourceName, limit: MAX_BSKY_PROFILES });
-	return nameResp?.data?.actors[profileIndex];
+	return nameResp?.data?.actors[0];
 };
